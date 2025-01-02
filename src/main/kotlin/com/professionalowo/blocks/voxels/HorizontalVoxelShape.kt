3376642f508b3b69b80bf1ac899c79f35d251ee6
professionalowo <martin.brandl1307@gmail.com>
2025-01-02 17:30:42 +0100

    voxel refactor

---
+++ src/main/kotlin/com/professionalowo/blocks/voxels/HorizontalVoxelShape.kt
@@ -0,0 +1,24 @@
+package com.professionalowo.blocks.voxels
+
+import net.minecraft.state.property.DirectionProperty
+import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+
+data class HorizontalVoxelShape(
+    val north: VoxelShape,
+    val east: VoxelShape,
+    val south: VoxelShape,
+    val west: VoxelShape
+) {
+    /**
+     * @return the VoxelShape associated with the Direction or north
+     */
+    operator fun get(direction: Direction?): VoxelShape =
+        when (direction) {
+            Direction.NORTH -> north
+            Direction.EAST -> east
+            Direction.SOUTH -> south
+            Direction.WEST -> west
+            else -> north
+        }
+}
