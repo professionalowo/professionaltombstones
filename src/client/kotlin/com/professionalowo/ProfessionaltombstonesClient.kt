f938fc0627639b16ff55a7cf597ed14259b7ca2a
professionalowo <martin.brandl1307@gmail.com>
2025-01-03 16:39:42 +0100

    refactored ProfessionaltombstonesClient.kt

--- src/client/kotlin/com/professionalowo/ProfessionaltombstonesClient.kt
+++ src/client/kotlin/com/professionalowo/ProfessionaltombstonesClient.kt
@@ -7,15 +7,26 @@ import com.professionalowo.renderer.TombstoneEntityRenderer
 import com.professionalowo.util.createLogger
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.minecraft.block.Block
 import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories
 
 object ProfessionaltombstonesClient : ClientModInitializer {
     private val logger = createLogger()
     override fun onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TOMBSTONE_BLOCK, RenderLayer.getCutout())
+        putCutouts(ModBlocks.TOMBSTONE_BLOCK)
 
         BlockEntityRendererFactories.register(ModBlockEntities.TOMBSTONE_BLOCK_ENTITY) { TombstoneEntityRenderer(it) }
         logger.info("Initialized Client for $MOD_ID")
+    }
+
+    private fun putCutouts(vararg blocks: Block) =
+        putRenderLayer(RenderLayer.getCutout())(blocks)
+
+
+    private fun putRenderLayer(layer: RenderLayer): (Array<out Block>) -> Unit {
+        return fun(blocks: Array<out Block>) {
+            BlockRenderLayerMap.INSTANCE.putBlocks(layer, *blocks)
+        }
     }
 }
