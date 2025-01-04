6ad71009cc1bc5baeeeb57d7c79bce221ec40315
professionalowo <martin.brandl1307@gmail.com>
2025-01-04 18:56:40 +0100

    refactored ModTabs.kt

--- src/main/kotlin/com/professionalowo/creative_tabs/ModTabs.kt
+++ src/main/kotlin/com/professionalowo/creative_tabs/ModTabs.kt
@@ -7,6 +7,7 @@ import com.professionalowo.util.modIdentifier
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.item.ItemConvertible
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
@@ -20,9 +21,7 @@ object ModTabs {
     val TOMBSTONE_GROUP = createGroup("tombstone_group") { ItemStack(ModBlocks.TOMBSTONE_BLOCK) }
 
     fun initialize() {
-        TOMBSTONE_GROUP.register {
-            it.add(ModBlocks.TOMBSTONE_BLOCK)
-        }
+        TOMBSTONE_GROUP.register(ModBlocks.TOMBSTONE_BLOCK)
 
         logger.info("Initialized ItemGroups")
     }
@@ -30,13 +29,12 @@ object ModTabs {
 
 data class Group(val key: RegistryKey<ItemGroup>, val itemGroup: ItemGroup) {
     fun register(registerFunc: (FabricItemGroupEntries) -> Unit) {
-        itemGroup.registerGroup(key, registerFunc)
-    }
-
-    private fun ItemGroup.registerGroup(key: RegistryKey<ItemGroup>, registerFunc: (FabricItemGroupEntries) -> Unit) {
-        Registry.register(Registries.ITEM_GROUP, key, this)
+        Registry.register(Registries.ITEM_GROUP, key, itemGroup)
         ItemGroupEvents.modifyEntriesEvent(key).register(registerFunc)
     }
+
+    fun register(vararg itemConvertibles: ItemConvertible) =
+        register { group -> itemConvertibles.forEach { group.add(it) } }
 }
 
 fun createGroup(id: String, iconSupplier: () -> ItemStack): Group {
