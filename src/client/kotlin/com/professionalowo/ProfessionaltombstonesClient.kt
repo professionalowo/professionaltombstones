b09d6ff5ca97d3943c7f0d9591bbccdb272d15d1
professionalowo <martin.brandl1307@gmail.com>
2025-01-05 15:23:09 +0100

    added in Initializer

--- src/client/kotlin/com/professionalowo/ProfessionaltombstonesClient.kt
+++ src/client/kotlin/com/professionalowo/ProfessionaltombstonesClient.kt
@@ -4,7 +4,6 @@ import com.professionalowo.Professionaltombstones.MOD_ID
 import com.professionalowo.blocks.ModBlockEntities
 import com.professionalowo.blocks.ModBlocks
 import com.professionalowo.renderer.TombstoneEntityRenderer
-import com.professionalowo.util.createLogger
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.minecraft.block.Block
