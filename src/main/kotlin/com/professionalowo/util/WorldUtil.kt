9eb038ae15947c76afa44f27fe701b0de292727d
professionalowo <martin.brandl1307@gmail.com>
2025-01-09 23:40:44 +0100

    removed unused function WorldUtil.kt

--- src/main/kotlin/com/professionalowo/util/WorldUtil.kt
+++ src/main/kotlin/com/professionalowo/util/WorldUtil.kt
@@ -1,16 +1,7 @@
 package com.professionalowo.util
 
-import net.minecraft.inventory.Inventory
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
-
-/**
- * Get the inventory from the BlockEntity at pos
- * @param pos the BlockPos of the BlockEntity
- * @return the Inventory of the BlockEntity, if available
- */
-fun World.getBlockInventory(pos: BlockPos) =
-    getBlockEntity(pos) as? Inventory
 
 /**
  * Checks if the block specified by pos is a solid block
