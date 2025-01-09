13ba2de7fecfdca9bbe7d60a363af71ca66135d3
professionalowo <martin.brandl1307@gmail.com>
2025-01-09 23:49:23 +0100

    added comments to VoxelShapeUtil.kt

--- src/main/kotlin/com/professionalowo/util/VoxelShapeUtil.kt
+++ src/main/kotlin/com/professionalowo/util/VoxelShapeUtil.kt
@@ -14,18 +14,32 @@ fun VoxelShape.or(other: VoxelShape): VoxelShape =
  * @return the rotated VoxelShape
  */
 fun VoxelShape.rotateShape(from: Direction, to: Direction): VoxelShape =
-    this.applyTransform(rotationTimes(from, to))
+    this.rotateNTimes(rotationTimes(from, to))
 
-
+/**
+ * @param from the start direction
+ * @param to the target direction
+ * @return how often to rotate on the Y-Axis to reach to
+ */
 private fun rotationTimes(from: Direction, to: Direction): Int = (to.horizontal - from.horizontal + 4) % 4
 
-private tailrec fun VoxelShape.applyTransform(times: Int): VoxelShape =
-    when (times) {
+/**
+ * Rotate this on the Y-Axis by n * 90%
+ * @param n the number of times to rotate this
+ * @return the result of rotating this n times
+ */
+private tailrec fun VoxelShape.rotateNTimes(n: Int): VoxelShape =
+    when (n) {
         0 -> this
         //transform the pair again, swap the pair
-        else -> transformSingleShape(this).applyTransform(times - 1)
+        else -> transformSingleShape(this).rotateNTimes(n.dec())
     }
 
+/**
+ * Rotate shape by 90% on the Y-Axis
+ * @param shape the shape to rotate
+ * @return the shape after being rotated
+ */
 private fun transformSingleShape(shape: VoxelShape): VoxelShape =
     buildList {
         shape.forEachBox { minX, minY, minZ, maxX, maxY, maxZ ->
