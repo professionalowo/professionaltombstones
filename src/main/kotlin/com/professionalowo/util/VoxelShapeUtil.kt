f8508b247d64f9ac109d56a3aceae4b8d2257365
professionalowo <martin.brandl1307@gmail.com>
2025-01-09 23:40:04 +0100

    updated VoxelShapeUtil.kt to use buildList full functional style

--- src/main/kotlin/com/professionalowo/util/VoxelShapeUtil.kt
+++ src/main/kotlin/com/professionalowo/util/VoxelShapeUtil.kt
@@ -14,47 +14,21 @@ fun VoxelShape.or(other: VoxelShape): VoxelShape =
  * @return the rotated VoxelShape
  */
 fun VoxelShape.rotateShape(from: Direction, to: Direction): VoxelShape =
-    if (from == to) this else {
-        val times = (to.horizontal - from.horizontal + 4) % 4
-        this.applyTransform(times)
-    }
+    this.applyTransform(rotationTimes(from, to))
 
 
-private fun VoxelShape.applyTransform(times: Int): VoxelShape =
-    applyTransform(times, Pair(this, VoxelShapes.empty())).first;
+private fun rotationTimes(from: Direction, to: Direction): Int = (to.horizontal - from.horizontal + 4) % 4
 
-private tailrec fun applyTransform(times: Int, buffer: Pair<VoxelShape, VoxelShape>): Pair<VoxelShape, VoxelShape> =
+private tailrec fun VoxelShape.applyTransform(times: Int): VoxelShape =
     when (times) {
-        0 -> buffer
+        0 -> this
         //transform the pair again, swap the pair
-        else -> applyTransform(times - 1, transformSinglePair(buffer).run { Pair(second, VoxelShapes.empty()) })
+        else -> transformSingleShape(this).applyTransform(times - 1)
     }
 
-
-private fun transformSinglePair(pair: Pair<VoxelShape, VoxelShape>): Pair<VoxelShape, VoxelShape> =
-    pair.run {
-        Pair(
-            first,
-            first.boxes()
-                .map { it.cuboidRotated() }
-                .fold(second) { acc, cuboid -> acc.or(cuboid) })
-    }
-
-private fun VoxelShape.boxes(): List<Box> {
-    val boxes = mutableListOf<Box>()
-    forEachBox { minX, minY, minZ, maxX, maxY, maxZ -> boxes.add(Box(minX, minY, minZ, maxX, maxY, maxZ)) }
-    return boxes
-}
-
-
-private data class Box(
-    val minX: Double,
-    val minY: Double,
-    val minZ: Double,
-    val maxX: Double,
-    val maxY: Double,
-    val maxZ: Double
-) {
-    fun cuboidRotated(): VoxelShape =
-        VoxelShapes.cuboid(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX) ?: VoxelShapes.empty()
-}
+private fun transformSingleShape(shape: VoxelShape): VoxelShape =
+    buildList {
+        shape.forEachBox { minX, minY, minZ, maxX, maxY, maxZ ->
+            add(VoxelShapes.cuboid(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX))
+        }
+    }.fold(VoxelShapes.empty()) { acc, cuboid -> acc.or(cuboid) }
