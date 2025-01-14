bd73006a580d08b8ba6dbaa99b236edb2756883e
professionalowo <martin.brandl1307@gmail.com>
2025-01-14 15:30:03 +0100

    removed scalar multiplication since it leaves the body of Integers

--- src/main/kotlin/com/professionalowo/util/Vec3iMath.kt
+++ src/main/kotlin/com/professionalowo/util/Vec3iMath.kt
@@ -7,7 +7,3 @@ operator fun Vec3i.plus(other: Vec3i): Vec3i = this.add(other)
 operator fun Vec3i.minus(other: Vec3i): Vec3i = this.subtract(other)
 
 operator fun Vec3i.unaryMinus(): Vec3i = Vec3i(-x, -y, -z)
-
-operator fun Vec3i.times(scalar: Int): Vec3i = this.multiply(scalar)
-
-operator fun Int.times(vector: Vec3i) = vector * this
