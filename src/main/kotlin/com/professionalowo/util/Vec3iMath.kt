8ad81b30e0877d25c5098355d22a5c2dbf58a338
professionalowo <martin.brandl1307@gmail.com>
2025-01-14 14:58:01 +0100

    added return type

--- src/main/kotlin/com/professionalowo/util/Vec3iMath.kt
+++ src/main/kotlin/com/professionalowo/util/Vec3iMath.kt
@@ -8,6 +8,6 @@ operator fun Vec3i.minus(other: Vec3i): Vec3i = this.subtract(other)
 
 operator fun Vec3i.unaryMinus(): Vec3i = Vec3i(-x, -y, -z)
 
-operator fun Vec3i.times(scalar: Int) = this.multiply(scalar)
+operator fun Vec3i.times(scalar: Int): Vec3i = this.multiply(scalar)
 
 operator fun Int.times(vector: Vec3i) = vector * this
