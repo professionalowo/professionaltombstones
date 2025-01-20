54b666b0d923c409a44baa1c64d5844bf6f9ee15
professionalowo <martin.brandl1307@gmail.com>
2025-01-20 17:14:18 +0100

    extended sword

--- src/main/kotlin/com/professionalowo/items/swords/WitherSwordItem.kt
+++ src/main/kotlin/com/professionalowo/items/swords/WitherSwordItem.kt
@@ -1,12 +1,36 @@
 package com.professionalowo.items.swords
 
+import com.professionalowo.util.modIdentifier
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.item.ItemStack
 import net.minecraft.item.SwordItem
 import net.minecraft.item.ToolMaterials
+import net.minecraft.item.tooltip.TooltipType
+import net.minecraft.text.Text
 import net.minecraft.util.Rarity
 
 class WitherSwordItem : SwordItem(
     ToolMaterials.NETHERITE, Settings().fireproof().rarity(Rarity.EPIC).attributeModifiers(
-        createAttributeModifiers(ToolMaterials.NETHERITE, 4, -2.4f)
+        createAttributeModifiers(ToolMaterials.NETHERITE, 5, -2.4f)
     )
 ) {
+    override fun postDamageEntity(stack: ItemStack, target: LivingEntity, attacker: LivingEntity) {
+        val effect = StatusEffectInstance(StatusEffects.WITHER, 60, 3, false, true, true)
+        target.addStatusEffect(effect)
+        super.postDamageEntity(stack, target, attacker)
+    }
+
+    override fun hasGlint(stack: ItemStack?): Boolean = true
+
+    override fun appendTooltip(
+        stack: ItemStack,
+        context: TooltipContext,
+        tooltip: MutableList<Text>,
+        type: TooltipType
+    ) {
+        tooltip.add(Text.translatable("wither_sword_tooltip"))
+        super.appendTooltip(stack, context, tooltip, type)
+    }
 }
