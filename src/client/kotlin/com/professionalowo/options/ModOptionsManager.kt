86f872387b6114c4d9669a07fc17a349d3f77e49
professionalowo <martin.brandl1307@gmail.com>
2025-01-24 14:53:48 +0100

    refactored a bit

--- src/client/kotlin/com/professionalowo/options/ModOptionsManager.kt
+++ src/client/kotlin/com/professionalowo/options/ModOptionsManager.kt
@@ -7,9 +7,9 @@ import net.minecraft.client.option.SimpleOption
 import net.minecraft.text.Text
 
 object ModOptionsManager {
-    fun getOptions(client: MinecraftClient?): Array<SimpleOption<*>> {
-        return arrayListOf(*getIntegratedWorldOptions(client)).filterNotNull().toTypedArray()
-    }
+    fun getOptions(client: MinecraftClient?): Array<SimpleOption<*>> =
+        arrayListOf(*getIntegratedWorldOptions(client)).filterNotNull().toTypedArray()
+
 
     private fun getIntegratedWorldOptions(client: MinecraftClient?): Array<SimpleOption<*>?> = arrayOf(
         getSpawnGravestoneOption(client),
@@ -22,15 +22,13 @@ object ModOptionsManager {
         if (client?.world != null && client.isIntegratedServerRunning) SimpleOption(
             createOptionKey("spawn_gravestone"),
             SimpleOption.emptyTooltip(),
-            { _: Text, value: Boolean -> Text.literal(value.toString()) },
+            { _, value -> Text.literal(value.toString()) },
             SimpleOption.BOOLEAN,
-            getSpawnGravestoneValue(client)
+            client.server?.gameRules?.get(ModGameRules.SPAWN_GRAVESTONE)?.get() ?: true
         ) {
             val server = client.server ?: return@SimpleOption
             server.gameRules?.get(ModGameRules.SPAWN_GRAVESTONE)?.set(it, server)
         } else null
 
-    private fun getSpawnGravestoneValue(client: MinecraftClient) =
-        client.server?.gameRules?.get(ModGameRules.SPAWN_GRAVESTONE)?.get() ?: true
 }
 
