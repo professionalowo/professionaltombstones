d891a3d32c91330be09b90b1f42081dfc4b7ca9c
professionalowo <martin.brandl1307@gmail.com>
2025-01-24 14:29:25 +0100

    added option to spawn gravestone or not

--- src/client/kotlin/com/professionalowo/screen/ModConfigScreen.kt
+++ src/client/kotlin/com/professionalowo/screen/ModConfigScreen.kt
@@ -1,21 +1,43 @@
 package com.professionalowo.screen
 
+import com.mojang.serialization.Codec
 import com.professionalowo.Professionaltombstones.MOD_ID
+import com.professionalowo.gamerules.ModGameRules
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.Screen
-import net.minecraft.client.gui.widget.ButtonWidget
+import net.minecraft.client.gui.screen.option.GameOptionsScreen
+import net.minecraft.client.gui.widget.Widget
+import net.minecraft.client.option.GameOptions
+import net.minecraft.client.option.SimpleOption
+import net.minecraft.server.integrated.IntegratedServer
 import net.minecraft.text.Text
 
 @Environment(EnvType.CLIENT)
-class ModConfigScreen(private val parent: Screen?) : Screen(Text.literal(MOD_ID)) {
-    override fun close() = client?.setScreen(parent) ?: Unit
+class ModConfigScreen(parent: Screen?) : GameOptionsScreen(
+    parent, MinecraftClient.getInstance().options,
+    Text.literal("$MOD_ID options")
+) {
 
-    override fun init() {
-        val closeButton = ButtonWidget.builder(Text.literal("Done")) {
-            close()
-        }.build()
-        closeButton.setPosition((width - closeButton.width) / 2, (height - closeButton.height) / 2)
-        addDrawableChild(closeButton)
+    override fun addOptions() {
+        if (client?.world != null && client?.isIntegratedServerRunning == true) {
+            body?.addAll(getSpawnGravestoneOption())
+        }
     }
+
+    private fun getSpawnGravestoneOption() = SimpleOption(
+        "options.$MOD_ID.spawn_gravestone",
+        SimpleOption.emptyTooltip(),
+        { _: Text, value: Boolean -> Text.literal(value.toString()) },
+        SimpleOption.BOOLEAN,
+        getSpawnGravestoneValue()
+    ) {
+        if (client?.world != null && client?.isIntegratedServerRunning == true) {
+            val server = client?.server ?: return@SimpleOption
+            client?.server?.gameRules?.get(ModGameRules.SPAWN_GRAVESTONE)?.set(it, server)
+        }
+    }
+
+    private fun getSpawnGravestoneValue() = client?.server?.gameRules?.get(ModGameRules.SPAWN_GRAVESTONE)?.get() ?: true
 }
