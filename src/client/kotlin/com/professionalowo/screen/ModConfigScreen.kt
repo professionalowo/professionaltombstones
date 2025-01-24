991b3360d6ed8fad8a42d11164442f259a96c9df
professionalowo <martin.brandl1307@gmail.com>
2025-01-24 12:39:20 +0100

    added things to the screen

--- src/client/kotlin/com/professionalowo/screen/ModConfigScreen.kt
+++ src/client/kotlin/com/professionalowo/screen/ModConfigScreen.kt
@@ -4,9 +4,18 @@ import com.professionalowo.Professionaltombstones.MOD_ID
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.ButtonWidget
 import net.minecraft.text.Text
 
 @Environment(EnvType.CLIENT)
 class ModConfigScreen(private val parent: Screen?) : Screen(Text.literal(MOD_ID)) {
     override fun close() = client?.setScreen(parent) ?: Unit
+
+    override fun init() {
+        val closeButton = ButtonWidget.builder(Text.literal("Close")) {
+            close()
+        }.build()
+        closeButton.setPosition((width - closeButton.width) / 2, (height - closeButton.height) / 2)
+        addDrawableChild(closeButton)
+    }
 }
