46f7ca8ce80571c22b5389cebccdfe25742357da
professionalowo <martin.brandl1307@gmail.com>
2025-01-25 22:15:27 +0100

    enhanced vials with sounds

--- src/main/kotlin/com/professionalowo/items/misc/FilledSoulVialItem.kt
+++ src/main/kotlin/com/professionalowo/items/misc/FilledSoulVialItem.kt
@@ -1,10 +1,16 @@
 package com.professionalowo.items.misc
 
+import net.minecraft.entity.Entity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.tooltip.TooltipType
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
+import net.minecraft.world.World
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class FilledSoulVialItem(settings: Settings) : Item(settings) {
     override fun appendTooltip(
@@ -19,5 +25,21 @@ class FilledSoulVialItem(settings: Settings) : Item(settings) {
         super.appendTooltip(stack, context, tooltip, type)
     }
 
-    override fun hasGlint(stack: ItemStack?): Boolean =true
+    override fun hasGlint(stack: ItemStack?): Boolean = true
+
+    override fun inventoryTick(stack: ItemStack, world: World, entity: Entity, slot: Int, selected: Boolean) {
+        if (!selected) return
+        val sounds = arrayOf(
+            SoundEvents.ENTITY_ZOMBIE_VILLAGER_AMBIENT,
+            SoundEvents.ENTITY_ZOMBIE_VILLAGER_HURT,
+            SoundEvents.ENTITY_ZOMBIE_VILLAGER_CONVERTED
+        )
+        val random = Random(System.nanoTime()).nextInt(0 until 200)
+        if (random >= 198) {
+            world.playSound(
+                entity, entity.blockPos, sounds.random(),
+                SoundCategory.BLOCKS, 1f, 1f
+            )
+        }
+    }
 }
