d92737de8627149af293fcf47fdcd1e42d13094e
professionalowo <martin.brandl1307@gmail.com>
2025-01-27 21:37:07 +0100

    removed english datagen

--- src/client/kotlin/com/professionalowo/ProfessionaltombstonesDataGenerator.kt
+++ src/client/kotlin/com/professionalowo/ProfessionaltombstonesDataGenerator.kt
@@ -2,7 +2,6 @@ package com.professionalowo
 
 import com.professionalowo.generator.ModBlockTagProvider
 import com.professionalowo.generator.ModEnchantmentGenerator
-import com.professionalowo.generator.ModEnglishLanguageProvider
 import com.professionalowo.generator.ModItemTagProvider
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
@@ -12,7 +11,6 @@ object ProfessionaltombstonesDataGenerator : DataGeneratorEntrypoint {
         val pack = generator.createPack()
         pack.addProvider { a, b -> ModBlockTagProvider(a, b) }
         pack.addProvider { a, b -> ModItemTagProvider(a, b) }
-        pack.addProvider { a, b -> ModEnglishLanguageProvider(a, b) }
         pack.addProvider { a, b -> ModEnchantmentGenerator(a, b) }
     }
 }
