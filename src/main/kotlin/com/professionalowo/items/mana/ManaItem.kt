59e52fcfa02c943e4db3a12e30575b6cf735d838
professionalowo <martin.brandl1307@gmail.com>
2025-01-28 15:18:47 +0100

    defaulted douse to pass

--- src/main/kotlin/com/professionalowo/items/mana/ManaItem.kt
+++ src/main/kotlin/com/professionalowo/items/mana/ManaItem.kt
@@ -11,7 +11,8 @@ import net.minecraft.world.World
 abstract class ManaItem(settings: Settings) : Item(settings) {
     abstract fun getManaCost(player: PlayerEntity): Int
 
-    protected open fun canPlayerUse(player: PlayerEntity): Boolean = getManaCost(player) < PlayerDataAccessor(player).mana
+    protected open fun canPlayerUse(player: PlayerEntity): Boolean =
+        getManaCost(player) < PlayerDataAccessor(player).mana
 
     protected open fun consumePlayerMana(player: PlayerEntity) {
         PlayerDataAccessor(player).decrementMana(getManaCost(player))
@@ -19,13 +20,13 @@ abstract class ManaItem(settings: Settings) : Item(settings) {
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> =
         if (canPlayerUse(user)) {
-            val result = doUse(world, user, hand)
-            if(result.result.isAccepted) {
-                consumePlayerMana(user)
+            doUse(world, user, hand).also {
+                if (it.result.isAccepted) {
+                    consumePlayerMana(user)
+                }
             }
-            result
         } else TypedActionResult.fail(user.getStackInHand(hand))
 
     open fun doUse(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> =
-        TypedActionResult.success(user.getStackInHand(hand))
+        TypedActionResult.pass(user.getStackInHand(hand))
 }
