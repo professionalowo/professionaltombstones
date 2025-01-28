9c0934ca1c3f8737eb3ec11acebd3468a55b4903
professionalowo <martin.brandl1307@gmail.com>
2025-01-28 10:27:43 +0100

    refactored signatures

--- src/main/kotlin/com/professionalowo/util/RegistryByteBufUtil.kt
+++ src/main/kotlin/com/professionalowo/util/RegistryByteBufUtil.kt
@@ -1,13 +1,13 @@
 package com.professionalowo.util
 
-import net.minecraft.network.RegistryByteBuf
+import net.minecraft.network.PacketByteBuf
 import net.minecraft.network.codec.PacketCodec
 import net.minecraft.network.codec.PacketCodecs
 
 
-fun <T> PacketCodec<RegistryByteBuf, T>.encodeList(buf: RegistryByteBuf, list: List<T>) =
+fun <B, T> PacketCodec<B, T>.encodeList(buf: B, list: List<T>) where B : PacketByteBuf =
     collect(PacketCodecs.toList()).encode(buf, list)
 
 
-fun <T> PacketCodec<RegistryByteBuf, T>.decodeList(buf: RegistryByteBuf): List<T> =
+fun <B, T> PacketCodec<B, T>.decodeList(buf: B): List<T> where B : PacketByteBuf =
     collect(PacketCodecs.toList()).decode(buf)
