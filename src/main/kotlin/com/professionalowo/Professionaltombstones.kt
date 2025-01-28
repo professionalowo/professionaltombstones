49b74801437538a3bb25c87bc63d18b7c0a02e68
professionalowo <martin.brandl1307@gmail.com>
2025-01-28 14:39:05 +0100

    added mana to PlayerDataAccessor

--- src/main/kotlin/com/professionalowo/Professionaltombstones.kt
+++ src/main/kotlin/com/professionalowo/Professionaltombstones.kt
@@ -6,17 +6,10 @@ import com.professionalowo.creative_tabs.ModTabs
 import com.professionalowo.enchantments.ModEnchantments
 import com.professionalowo.gamerules.ModGameRules
 import com.professionalowo.items.ModItems
-import com.professionalowo.player_data.PlayerDataWrapper
 import com.professionalowo.recipes.ModRecipeSerializers
 import com.professionalowo.recipes.ModRecipieTypes
 import com.professionalowo.sound.ModSoundEvents
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
-import net.minecraft.block.BlockState
-import net.minecraft.block.entity.BlockEntity
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
 
 object Professionaltombstones : Initializer(), ModInitializer {
     const val MOD_ID = "professionaltombstones"
---
+++ src/main/kotlin/com/professionalowo/player_data/PlayerDataAccessor.kt
@@ -0,0 +1,25 @@
+package com.professionalowo.player_data
+
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.nbt.NbtCompound
+
+class PlayerDataAccessor(val player: PlayerEntity) : IPlayerDataHandler {
+    companion object {
+        const val MANA_VALUE_KEY = "mana_value_key"
+        const val MAX_MANA_KEY = "max_mana_key"
+    }
+
+    val handler: IPlayerDataHandler =
+        player as? IPlayerDataHandler ?: throw IllegalStateException("Mixins might not have been initialized")
+
+    override fun getNbtData(): NbtCompound = handler.getNbtData()
+
+    var mana: Int
+        get() = getNbtData().getInt(MANA_VALUE_KEY)
+        set(value) = getNbtData().putInt(MANA_VALUE_KEY, value)
+
+    var maxMana: Int
+        get() = getNbtData().getInt(MAX_MANA_KEY)
+        set(value) = getNbtData().putInt(MAX_MANA_KEY, value)
+
+}
